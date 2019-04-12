a03bef9af664e7b5cdb09692689d74ad587a6420
Linus S <LinusS1@users.noreply.github.com>
2019-04-12 08:48:42 -0700

    Use convelusion nn

--- main.py
+++ main.py
@@ -1,143 +1,68 @@
-"""This file will:
- - Download the data
- - run through the analyzers
- - save the data from the analyzers"""
-from twitter_analysis import get_tweets
+from keras.preprocessing import sequence
 from keras.models import Sequential
-import csv
-from keras.layers import Dense, Embedding, GlobalAveragePooling1D, Dropout
-import numpy as np
-import pandas as pd
-from keras.preprocessing.text import Tokenizer
-from keras.preprocessing.sequence import pad_sequences
-from keras.utils import to_categorical
-"""
-poli_username = [
-    # "realDonaldTrump",
-    #                  "SecPompeo",
-    #                  "MarkWarner",
-    #                  "timkaine",
-    #                  "ChrisVanHollen",
-    #                  "SenatorCardin",
-    "SenFeinstein",
-    "KamalaHarris",
-    "RepFredUpton",
-    "Repjimbanks",
-]  # List of politition usernames
-sci_username = [
-    # "zenbrainest",
-    #                 "K_dele",
-    #                 "betenoire1",
-    #                 "doc2r06",
-    #                 "sebatlab",
-    "anne_churchland",
-    "kaymtye",
-    "alfairhall",
-    "bita137",
-    "CarolynBertozzi"
-]  # List of scientists usernames
+from keras.layers import Dense, Dropout, Activation
+from keras.layers import Embedding
+from keras.layers import Conv1D, GlobalMaxPooling1D
+from keras.datasets import imdb
 
-poli_text = []  # The texts from politions
-sci_text = []  # The texts from scientists
+# settings:
+max_features = 4000
+maxlen = 400
+batch_size = 32
+embedding_dims = 50
+filters = 250
+kernel_size = 3
+epochs = 2
 
-job_usernames = [poli_username, sci_username]
+print('Loading data...')
+(x_train, y_train), (x_test, y_test) = imdb.load_data(num_words=max_features)
 
-# Get tweets
-for job in job_usernames:
-    for username in job:
-        print("Getting text for " + username)
-        # Download 30 tweets from each username
-        for tweet in get_tweets(username, tweets=30, retweets=False):
-            if tweet.get("orginaluser") == username:
-                if job == poli_username:
-                    poli_text.append(tweet.get("text"))
-                elif job == sci_username:
-                    sci_text.append(tweet.get("text"))
+print('Pad sequences (samples x time)')
+x_train = sequence.pad_sequences(x_train, maxlen=maxlen)
+x_test = sequence.pad_sequences(x_test, maxlen=maxlen)
 
-job_text = {"poli": poli_text, "sci": sci_text}
-print("Done Downloading data.")
-print(job_text)
-
-with open('test_data.csv', mode='w') as test_data:
-    test_data_writer = csv.writer(test_data, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-    for text in job_text["poli"]:
-        test_data_writer.writerow([text, "poli"])
-    for text in job_text["sci"]:
-        test_data_writer.writerow([text, "sci"])
-"""
-# PREPROCESS
-
-
-
-
-train_data_source = 'train_data.csv'
-test_data_source = 'test_data.csv'
-train_df = pd.read_csv(train_data_source, header=None)
-test_df = pd.read_csv(test_data_source, header=None)
-
-# convert string to lower case
-train_texts = train_df[0].values
-train_texts = [s.lower() for s in train_texts]
-test_texts = test_df[0].values
-test_texts = [s.lower() for s in test_texts]
-# =======================Convert string to index================
-
-
-# Tokenizer
-tk = Tokenizer(num_words=None, char_level=True, oov_token='UNK')
-tk.fit_on_texts(train_texts)
-# If we already have a character list, then replace the tk.word_index
-# If not, just skip below part
-# -----------------------Skip part start--------------------------
-# construct a new vocabulary
-alphabet = "abcdefghijklmnopqrstuvwxyz0123456789-,;.!?:'\"/\\|_@#$%^&*~`+-=<>()[]{}"
-char_dict = {}
-for i, char in enumerate(alphabet):
-    char_dict[char] = i + 1
-
-# Use char_dict to replace the tk.word_index
-tk.word_index = char_dict
-# Add 'UNK' to the vocabulary
-tk.word_index[tk.oov_token] = max(char_dict.values()) + 1
-# -----------------------Skip part end----------------------------
-# Convert string to index
-train_sequences = tk.texts_to_sequences(train_texts)
-test_texts = tk.texts_to_sequences(test_texts)
-# Padding
-train_data = pad_sequences(train_sequences, maxlen=280, padding='post')
-test_data = pad_sequences(test_texts, maxlen=280, padding='post')
-# Convert to numpy array
-train_data = np.array(train_data)
-test_data = np.array(test_data)
-# =======================Get classes================
-train_classes = train_df[1].values
-train_class_list = [x - 1 for x in train_classes]
-test_classes = test_df[1].values
-test_class_list = [x - 1 for x in test_classes]
-
-
-# train_classes = to_categorical(train_class_list)
-# test_classes = to_categorical(test_class_list)
-print(train_classes, train_data)
-print(test_classes, test_data)
-# Build model
-
+print('Build model...')
 model = Sequential()
-model.add(Embedding(10000, 16))
-model.add(GlobalAveragePooling1D())
-model.add(Dense(units=16, activation='relu'))
-model.add(Dense(units=16, activation='relu'))
-model.add(Dense(1, activation='sigmoid'))
 
-# Test model
-model.compile(optimizer='rmsprop',
-              loss='binary_crossentropy',
+# we start off with an efficient embedding layer which maps
+# our vocab indices into embedding_dims dimensions
+model.add(Embedding(max_features,
+                    embedding_dims,
+                    input_length=maxlen))
+model.add(Dropout(0.2))
+
+# we add a Convolution1D, which will learn filters
+# word group filters of size filter_length:
+model.add(Conv1D(filters,
+                 kernel_size,
+                 padding='valid',
+                 activation='relu',
+                 strides=1))
+# we use max pooling:
+model.add(GlobalMaxPooling1D())
+
+
+# Add the changeable layers
+model.add(Dense(16))
+model.add(Dropout(0.2))
+model.add(Activation('relu'))
+
+# We project onto a single unit output sigmoid layer
+model.add(Dense(1))
+model.add(Activation('sigmoid'))
+
+model.compile(loss='binary_crossentropy',
+              optimizer='adam',
               metrics=['accuracy'])
 
-print("Train model")
-model.fit(train_data, train_classes, epochs=30, batch_size=64)
+model.fit(x_train, y_train,
+          batch_size=batch_size,
+          epochs=epochs,
+          validation_data=(x_test, y_test))
 
-print("Testing model")
-loss_and_metrics = model.evaluate(test_data, test_classes)
-print("Results:")
-print(loss_and_metrics)
+print(model.summary())
+
+results = model.evaluate(x_test, y_test)
+
+print(results)
+input("Press enter to continue")
